6f8bac6ea3a41d06fc477e83b12f48e5a2c5d813
Nero Tran <tranhuq@miamioh.edu>
2021-11-08 21:19:39 -0500

    update

--- Madlib.cs
+++ Madlib.cs
@@ -1,4 +1,24 @@
 // TODO:  Write the Madlib program in C#
 //
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Enter a Name: ");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine("Enter a Color: ");
+        string color = Console.ReadLine();
 
+        Console.WriteLine("Enter a Noun (pl): ");
+        string noun = Console.ReadLine();
+		Console.WriteLine();
+
+        string poem = $"Roses are {color}\n{noun} are blue,\n{name} loves you.";
+		
+		Console.WriteLine(poem);
+	}
+}
 
